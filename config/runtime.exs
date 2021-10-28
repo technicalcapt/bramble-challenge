dff72106f001b3bb4fdef18e53c110bd7801f09a
technicalcapt <technicalcapt@gmail.com>
2021-10-29 00:54:12 +0700

    fix docker build - update runtime config and elixir version

--- config/runtime.exs
+++ config/runtime.exs
@@ -41,6 +41,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
+    check_origin: false,
     secret_key_base: secret_key_base
 
   # ## Using releases
@@ -48,7 +49,7 @@ if config_env() == :prod do
   # If you are doing OTP releases, you need to instruct Phoenix
   # to start each relevant endpoint:
   #
-  #     config :bramble_challenge, BrambleChallengeWeb.Endpoint, server: true
+  config :bramble_challenge, BrambleChallengeWeb.Endpoint, server: true
   #
   # Then you can assemble a release by calling `mix release`.
   # See `mix help release` for more information.
